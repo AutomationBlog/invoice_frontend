4bd2a60d5bfa9bf1af26eb79c2be5e77e015fe55
automationblog <sawin24@gmail.com>
2024-11-01 14:56:53 +0530

    signup page updated

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,10 +1,24 @@
+import { Link } from "react-router-dom";
+import heroImage from "../assets/hero-invoice-now-2.png";
+
 const HeroSection = () => {
   return (
     <div>
-      <h1>Invoice App</h1>
-      <p>
-        <a href="/login">Login</a>
-      </p>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img src={heroImage} className="max-w-sm rounded-lg shadow-2xl" />
+          <div>
+            <h1 className="text-5xl font-bold">Invoice Now</h1>
+            <p className="py-6">
+              Manage your invoices with ease. Create, edit, and print invoices
+              in just a few clicks. And, get paid on time.
+            </p>
+            <Link to="/signup">
+              <button className="btn btn-primary">Get Started</button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,43 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="hero min-h-screen bg-base-200 flex items-center justify-center">
+      <div className="card bg-base-100 w-96 shadow-xl">
+        <p className="text-center text-[22px] font-bold pt-2">Login</p>
+        <div className="card-body items-center text-center">
+          <form>
+            <div className="form-control">
+              <input
+                type="text"
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control pt-5">
+              <input
+                type="text"
+                placeholder="password"
+                className="input input-bordered"
+              />
+            </div>
+          </form>
+          <div className="card-actions flex-col items-center justify-center pt-3">
+            <button className="btn btn-primary">Login</button>
+            <p>
+              Don't have an account?
+              <Link to="/signup">
+                <span className="hover:underline text-blue-800 font-bold">
+                  {" "}
+                  Sign Up
+                </span>
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
