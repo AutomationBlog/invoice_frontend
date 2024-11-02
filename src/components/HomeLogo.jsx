f83a9597dd7995916dbc6c26c4db60d1fe6c91b5
automationblog <sawin24@gmail.com>
2024-11-02 18:08:59 +0530

    logo updated

--- src/components/HomeLogo.jsx
+++ src/components/HomeLogo.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/invoice-now-logo.png";
+import logo from "../assets/logo.png";
 const HomeLogo = () => {
   return (
     <div className="flex-1">
